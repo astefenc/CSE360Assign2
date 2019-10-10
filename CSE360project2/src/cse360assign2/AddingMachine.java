d26bf56e4e8cc4a77470043fe453eb68465924ad
Alyssa <astefenc@asu.edu>
2019-10-09 22:54:12 -0700

    added proper documentation

--- CSE360project2/src/cse360assign2/AddingMachine.java
+++ CSE360project2/src/cse360assign2/AddingMachine.java
@@ -1,3 +1,12 @@
+/*
+ * Alyssa Stefencavage 
+ * Class number: 70641
+ * Assignment 2
+ * This is the adding machine class that will add, subtract,
+ * and return the total. To implement the toString method 
+ * I added strings for each method that updates until the toString is called
+ * This file is committed to my repository on git hub.
+ */
 package cse360assign2;
 import java.lang.String;
 public class AddingMachine {
@@ -15,19 +24,19 @@ public class AddingMachine {
 	
 	public void add (int value) {
 		total = total + value; //added to total
-		output = output + " + " + value;
+		output = output + " + " + value; //add to output
 		
 	}
 	
 	public void subtract (int value) {
 		total = total - value; //subtracted from total
-		output = output + " - " + value;
+		output = output + " - " + value; //add to output
 
 		
 	}
 	
 	public String toString () {
-		return output;
+		return output; //returns the string of the history of calculations
 	}
 	
 	public void clear() {
